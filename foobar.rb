c09ab2768e947d78781bd4faa17174cc94c3a9d1
Jim <jimhollingworth1@gmail.com>
2016-09-20 17:13:06 -0700

    Jim Hollingworth homework

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,19 @@
 class Foobar
-
-  def self.baz(a)
+    
+    def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-  end
+    a.map! {|item| item.to_i}
+    a.map! {|item| item + 2}
+    a.delete_if {|item| item%2 == 1 }
+    a = a.uniq
+    a.delete_if {|item| item > 10}
+    sum = 0
+    a.each { |item| sum+=item }
+    return sum
+    
 end
 
 
+
+end
